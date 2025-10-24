cb99d00780a5a89f5a0c09e75697dd0345099200
CaitlynStocker <caitlyn.stocker@octopus.com>
2025-10-24 10:06:02 +1000

    Adjust jest config to match login action

--- jest.config.js
+++ jest.config.js
@@ -11,38 +11,13 @@ module.exports = {
             transform: {
                 ".(ts|js)": "ts-jest",
             },
-            testRegex: "__tests__/.*\\.(test|spec)\\.(ts)$",
+            testMatch: ['**/__tests__/**/*.(test|spec).ts', '**/*.test.ts'],
             moduleDirectories: ["<rootDir>/src/", "<rootDir>/node_modules"],
             moduleFileExtensions: ["ts", "js"],
             transformIgnorePatterns: [
                 "node_modules/(?!(until-async|msw)/)"
             ],
-            // setupFilesAfterEnv: ["jest-expect-message", "jest-extended"],
             resetMocks: true,
         },
     ],
 };
-
-
-// module.exports = {
-//     preset: 'ts-jest',
-//     testEnvironment: 'node',
-//     collectCoverage: true,
-//     collectCoverageFrom: [
-//         'src/**/*.ts',
-//         '!src/**/*.test.ts',
-//         '!src/**/*.d.ts'
-//     ],
-//     coverageDirectory: 'coverage',
-//     coverageReporters: ['text', 'lcov', 'html'],
-//     testMatch: ['**/*.test.ts'],
-//     moduleFileExtensions: ['ts', 'js', 'json'],
-//     verbose: true,
-//     reporters: [
-//         'default',
-//         ['jest-junit', {
-//             outputDirectory: 'test-results',
-//             outputName: 'jest-junit.xml',
-//         }]
-//     ]
-// };
